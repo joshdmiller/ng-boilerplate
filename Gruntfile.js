bf177ee548ed97a2dbfd9a6964b2b2033f183b97
bcarrell <brandoncarrell@gmail.com>
2013-06-29 12:36:21 -0400

    Adding vendor CSS build to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -128,6 +128,16 @@ module.exports = function ( grunt ) {
           }
         ]
       },
+      build_vendorcss: {
+        files: [
+          {
+            src: [ '<%= vendor_files.css %>' ],
+            dest: '<%= build_dir %>/',
+            cwd: '.',
+            expand: true
+          }
+        ]
+      },
       compile_assets: {
         files: [
           {
@@ -541,7 +551,7 @@ module.exports = function ( grunt ) {
   grunt.registerTask( 'build', [
     'clean', 'html2js', 'jshint', 'coffeelint', 'coffee','recess:build',
     'copy:build_assets', 'copy:build_appjs', 'copy:build_vendorjs',
-    'index:build', 'karmaconfig', 'karma:continuous' 
+    'copy:build_vendorcss', 'index:build', 'karmaconfig', 'karma:continuous' 
   ]);
 
   /**
