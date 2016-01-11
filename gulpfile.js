888b1de3294334bb06e1f39e63caf1c973e40a4c
chris <chris@chris.com>
2016-01-11 18:11:22 +0000

    WIP - adding gulp tasks to combine files and copy etc

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 var gulp = require('gulp');
 var Server = require('karma').Server;
+var less = require('gulp-less');
+var path = require('path');
+var inject = require('gulp-inject');
+
+var vendorJS = [
+    'vendor/angular/angular.js',
+    'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
+    'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js',
+    'vendor/angular-ui-router/release/angular-ui-router.js',
+    'vendor/angular-ui-utils/modules/route/route.js'
+];
 
 gulp.task('default', ['test']);
-
-gulp.task('karma-watch', function (done) {
-    new Server({
-        configFile: __dirname + '/test.conf.js',
-    },  done).start();
-});
 
 gulp.task('test', function (done) {
     new Server({
@@ -16,6 +21,49 @@ gulp.task('test', function (done) {
     }).start();
 });
 
+gulp.task('karma-watch', function (done) {
+    new Server({
+        configFile: __dirname + '/test.conf.js',
+    },  done).start();
+});
+
 gulp.task('watch', function() {
     gulp.watch('src/**/*.js', ['karma-watch']);
+});
+
+gulp.task('build', ['less', 'index', 'copy-files']);
+
+gulp.task('less', function () {
+    return gulp.src('src/less/main.less')
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(gulp.dest('build/assets'));
+});
+
+gulp.task('copy-files', ['copy-vendor-files'], function(){
+
+    var filesToCopy = ['src/**/*.js', '!src/**/*.spec.js', 'src/**/*.html'];
+
+    return gulp.src(filesToCopy)
+        .pipe(gulp.dest('build/'));
+});
+
+gulp.task('copy-vendor-files', function(){
+
+    var filesToCopy = vendorJS;
+
+    return gulp.src(filesToCopy)
+        .pipe(gulp.dest('build/vendor/'));
+});
+
+gulp.task('index', function () {
+    var includes = ['./src/**/*.js', '!./src/**/*.spec.js', './build/assets/main.css'];
+    includes = includes.concat(vendorJS);
+
+    var sources = gulp.src(includes, {read: false});
+
+    return gulp.src('./src/index.html')
+        .pipe(inject(sources, {relative: true}))
+        .pipe(gulp.dest('./src'));
 });
